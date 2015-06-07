67897f0ea3d23a66a83317fc593c5c6f52a4b00f
joshuadbaker <jbakercoder@gmail.com>
2015-06-07 15:36:21 -0400

    added photo upload still need to resize

--- Gemfile
+++ Gemfile
@@ -1,9 +1,12 @@
 source 'https://rubygems.org'
 gem 'devise'
 gem 'pg'
-
+gem "fog-aws"
+gem 'fog'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
+# Used to upload photos to site for Users
+gem 'carrierwave'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  # Loads photos through CarrierWave
+  mount_uploader :photo, PhotoUploader
+
   has_many :projects
   has_many :events_users
   has_many :events, through: :events_users
@@ -12,7 +16,6 @@ class User < ActiveRecord::Base
 
   has_many :topics_users
   has_many :topics, through: :topics_users
-
 
   def suggested_events
     #Event.joins(:skills).where({skills: {id: self.skills}}).distinct.limit(5)
