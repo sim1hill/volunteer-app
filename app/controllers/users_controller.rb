1c00fdeb3051eedbb9e29e7fbf268040ba6ea1cc
sim1hill <simone.c.hill@gmail.com>
2015-06-12 08:24:22 -0400

    edit form not working yet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   end
 
   def update
+    binding.pry
     @user = User.find(params[:id])
     @user.update(users_params)
     @skill = Skill.find(params[:skills_user][:skill])
